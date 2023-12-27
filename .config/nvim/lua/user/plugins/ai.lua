f3c4154d0579fff108791ff0e1478aeaeb40c8f5
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2023-12-27 19:54:42 +1100

    add cmp to command mode, move ai stuff around

---
+++ .config/nvim/lua/user/plugins/ai.lua
@@ -0,0 +1,316 @@
+return {
+  -- {
+  --   "nomnivore/ollama.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   -- All the user commands added by the plugin
+  --   cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+  --   keys = {
+  --     -- -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+  --     -- {
+  --     --   "<leader>Oo",
+  --     --   ":<c-u>lua require('ollama').prompt()<cr>",
+  --     --   desc = "ollama prompt",
+  --     --   mode = { "n", "v" },
+  --     -- },
+  --     --
+  --     -- -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+  --     -- {
+  --     --   "<leader>OG",
+  --     --   ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+  --     --   desc = "ollama Generate Code",
+  --     --   mode = { "n", "v" },
+  --     -- },
+  --   },
+  --   opts = {
+  --     model = "codellama:13b",
+  --     url = os.getenv "OLLAMA_API_HOST",
+  --     -- View the actual default prompts in ./lua/ollama/prompts.lua
+  --     prompts = {
+  --       -- Sample_Prompt = {
+  --       --   prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+  --       --   input_label = "> ",
+  --       --   model = "codellama:13b",
+  --       --   action = "display",
+  --       -- },
+  --     },
+  --   },
+  -- },
+  {
+    "huynle/ogpt.nvim",
+    event = "VeryLazy",
+    config = function() require("ogpt").setup() end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
+-- return {
+--   {
+--     "Exafunction/codeium.vim",
+--     event = "User AstroFile",
+--     keys = {
+--       {
+--         "<leader>;",
+--         function()
+--           if vim.g.codeium_enabled == true then
+--             vim.cmd "CodeiumDisable"
+--           else
+--             vim.cmd "CodeiumEnable"
+--           end
+--         end,
+--         desc = "Toggle Codeium",
+--       },
+--     },
+--     config = function()
+--       vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+--       vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+--     end,
+--   },
+--   {
+--     "rebelot/heirline.nvim",
+--     -- WARN: codeium support
+--     dependencies = {
+--       "Exafunction/codeium.vim",
+--     },
+--     event = "BufEnter",
+--     opts = function()
+--       local status = require "astronvim.utils.status"
+--       -- WARN: codeium support
+--       local codeium_status = {
+--         provider = function() return "🤖 " .. vim.api.nvim_eval "codeium#GetStatusString()" end,
+--         update = true,
+--       }
+--       return {
+--         opts = {
+--           disable_winbar_cb = function(args)
+--             return not require("astronvim.utils.buffer").is_valid(args.buf)
+--               or status.condition.buffer_matches({
+--                 buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+--                 filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+--               }, args.buf)
+--           end,
+--         },
+--         statusline = { -- statusline
+--           hl = { fg = "fg", bg = "bg" },
+--           status.component.mode(),
+--           status.component.git_branch(),
+--           status.component.file_info { filetype = {}, filename = false, file_modified = false },
+--           status.component.git_diff(),
+--           status.component.diagnostics(),
+--           status.component.diagnostics(),
+--           status.component.fill(),
+--           status.component.cmd_info(),
+--           status.component.fill(),
+--           --WARN: codeium support
+--           codeium_status,
+--           status.component.lsp(),
+--           status.component.treesitter(),
+--           status.component.nav(),
+--           status.component.mode { surround = { separator = "right" } },
+--         },
+--         winbar = { -- winbar
+--           init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+--           fallthrough = false,
+--           {
+--             condition = function() return not status.condition.is_active() end,
+--             status.component.separated_path(),
+--             status.component.file_info {
+--               file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+--               file_modified = false,
+--               file_read_only = false,
+--               hl = status.hl.get_attributes("winbarnc", true),
+--               surround = false,
+--               update = "BufEnter",
+--             },
+--           },
+--           status.component.breadcrumbs { hl = status.hl.get_attributes("winbar", true) },
+--         },
+--         tabline = { -- bufferline
+--           { -- file tree padding
+--             condition = function(self)
+--               self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+--               return status.condition.buffer_matches({
+--                 filetype = {
+--                   "NvimTree",
+--                   "OverseerList",
+--                   "aerial",
+--                   "dap%-repl",
+--                   "dapui_.",
+--                   "edgy",
+--                   "neo%-tree",
+--                   "undotree",
+--                 },
+--               }, vim.api.nvim_win_get_buf(self.winid))
+--             end,
+--             provider = function(self) return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1) end,
+--             hl = { bg = "tabline_bg" },
+--           },
+--           status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
+--           status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
+--           { -- tab list
+--             condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
+--             status.heirline.make_tablist { -- component for each tab
+--               provider = status.provider.tabnr(),
+--               hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
+--             },
+--             { -- close button for current tab
+--               provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+--               hl = status.hl.get_attributes("tab_close", true),
+--               on_click = {
+--                 callback = function() require("astronvim.utils.buffer").close_tab() end,
+--                 name = "heirline_tabline_close_tab_callback",
+--               },
+--             },
+--           },
+--         },
+--         statuscolumn = vim.fn.has "nvim-0.9" == 1 and {
+--           init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+--           status.component.foldcolumn(),
+--           status.component.fill(),
+--           status.component.numbercolumn(),
+--           status.component.signcolumn(),
+--         } or nil,
+--       }
+--     end,
+--     config = require "plugins.configs.heirline",
+--   },
+-- }
+
+-- Cool idea but I don't think this works that great, this could totally be a me problem
+-- return {
+--   {
+--     "tzachar/cmp-ai",
+--     dependencies = "nvim-lua/plenary.nvim",
+--     config = function()
+--       local cmp_ai = require "cmp_ai.config"
+--
+--       cmp_ai:setup {
+--         max_lines = 100,
+--         provider = "Ollama",
+--         provider_options = {
+--           model = "codellama:13b",
+--           base_url = os.getenv "OLLAMA_API_HOST" .. "/api/generate",
+--         },
+--         notify = true,
+--         notify_callback = function(msg)
+--           vim.notify(msg, vim.log.levels.INFO, {
+--             title = "OLlama",
+--             render = "compact",
+--           })
+--         end,
+--         run_on_every_keystroke = true,
+--         ignored_file_types = {
+--           -- lua = true
+--         },
+--       }
+--     end,
+--   },
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "saadparwaiz1/cmp_luasnip",
+--       "hrsh7th/cmp-buffer",
+--       "hrsh7th/cmp-path",
+--       "hrsh7th/cmp-nvim-lsp",
+--       "tzachar/cmp-ai",
+--     },
+--     event = "InsertEnter",
+--     opts = function()
+--       local cmp = require "cmp"
+--       local snip_status_ok, luasnip = pcall(require, "luasnip")
+--       local lspkind_status_ok, lspkind = pcall(require, "lspkind")
+--       local utils = require "astronvim.utils"
+--       if not snip_status_ok then return end
+--       local border_opts = {
+--         border = "rounded",
+--         winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+--       }
+--
+--       local function has_words_before()
+--         local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+--         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+--       end
+--
+--       return {
+--         enabled = function()
+--           local dap_prompt = utils.is_available "cmp-dap" -- add interoperability with cmp-dap
+--             and vim.tbl_contains(
+--               { "dap-repl", "dapui_watches", "dapui_hover" },
+--               vim.api.nvim_get_option_value("filetype", { buf = 0 })
+--             )
+--           if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" and not dap_prompt then return false end
+--           return vim.g.cmp_enabled
+--         end,
+--         preselect = cmp.PreselectMode.None,
+--         formatting = {
+--           fields = { "kind", "abbr", "menu" },
+--           format = lspkind_status_ok and lspkind.cmp_format(utils.plugin_opts "lspkind.nvim") or nil,
+--         },
+--         snippet = {
+--           expand = function(args) luasnip.lsp_expand(args.body) end,
+--         },
+--         duplicates = {
+--           cmp_ai = 1,
+--           nvim_lsp = 1,
+--           luasnip = 1,
+--           cmp_tabnine = 1,
+--           buffer = 1,
+--           path = 1,
+--         },
+--         confirm_opts = {
+--           behavior = cmp.ConfirmBehavior.Replace,
+--           select = false,
+--         },
+--         window = {
+--           completion = cmp.config.window.bordered(border_opts),
+--           documentation = cmp.config.window.bordered(border_opts),
+--         },
+--         mapping = {
+--           ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+--           ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+--           ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+--           ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+--           ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+--           ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+--           ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+--           ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+--           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+--           ["<C-y>"] = cmp.config.disable,
+--           ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
+--           ["<CR>"] = cmp.mapping.confirm { select = false },
+--           ["<Tab>"] = cmp.mapping(function(fallback)
+--             if cmp.visible() then
+--               cmp.select_next_item()
+--             elseif luasnip.expand_or_jumpable() then
+--               luasnip.expand_or_jump()
+--             elseif has_words_before() then
+--               cmp.complete()
+--             else
+--               fallback()
+--             end
+--           end, { "i", "s" }),
+--           ["<S-Tab>"] = cmp.mapping(function(fallback)
+--             if cmp.visible() then
+--               cmp.select_prev_item()
+--             elseif luasnip.jumpable(-1) then
+--               luasnip.jump(-1)
+--             else
+--               fallback()
+--             end
+--           end, { "i", "s" }),
+--         },
+--         sources = cmp.config.sources {
+--           { name = "nvim_lsp", priority = 1000 },
+--           { name = "luasnip", priority = 750 },
+--           { name = "buffer", priority = 500 },
+--           { name = "path", priority = 250 },
+--           { name = "cmp_ai", priority = 300 },
+--         },
+--       }
+--     end,
+--   },
+-- }
