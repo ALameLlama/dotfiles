324a8a412649f06faa16a62e82b1bf98ba0ee526
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-02-24 03:12:30 +0000

    feat: more ai

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -4,8 +4,12 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- Theme
   { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- AI
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.chatgpt-nvim" },
 
   -- Packs
   { import = "astrocommunity.pack.bash" },
