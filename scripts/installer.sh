a82abca616ab4913772d93c1e266804c55665c53
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-03-05 11:26:29 +0000

    fix: change shell

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -359,7 +359,7 @@ install_oh_my_zsh() {
 		git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 		git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-		chsh -s /usr/bin/zsh
+		sudo chsh -s /usr/bin/zsh "$USER"
 
 		msg_succ "oh my zsh has been installed."
 	else
