7db55ca97c16d7c281b798e85af99302052b0892
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-04-20 13:51:05 +0000

    feat: enable files

--- .config/nvim/lua/plugins/htmx.lua
+++ .config/nvim/lua/plugins/htmx.lua
@@ -1,15 +1,15 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-local utils = require("astronvim.utils")
-
 ---@type LazySpec
 return {
-	{
-		"ThePrimeagen/htmx-lsp",
-		version = "^0.1",
-		ft = { "htmx" },
-		init = function()
-			astronvim.lsp.setup = utils.list_insert_unique(astronvim.lsp.setup, "htmx")
-		end,
-	},
+  {
+    "ThePrimeagen/htmx-lsp",
+    version = "^0.1",
+    ft = { "htmx" },
+    lazy = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "htmx" },
+    },
+  },
 }
