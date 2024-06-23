2154a703008e5619f5c06031098c33f66c844b69
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-06-23 15:45:55 +1000

    feat: nvm

--- .config/nvim/lua/plugins/personal.lua
+++ .config/nvim/lua/plugins/personal.lua
@@ -1,17 +1,17 @@
 ---@type LazySpec
 return {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-		event = "User AstroFile",
-		cmd = { "TodoQuickFix" },
-		keys = {
-		  { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
-		},
-	  },
-	  -- TODO: NVIM 0.10, This is native, we can remove this.
-	  {
-		"ojroques/nvim-osc52",
-	  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  -- TODO: NVIM 0.10, This is native, we can remove this.
+  {
+    "ojroques/nvim-osc52",
+  },
 }
