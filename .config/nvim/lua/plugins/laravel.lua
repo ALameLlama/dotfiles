ad8c3ed01771e038b5e5bf032ff20b3d34b91818
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2024-07-15 13:07:23 +0000

    feat: more laravel stuff

---
+++ .config/nvim/lua/plugins/laravel.lua
@@ -0,0 +1,69 @@
+---@type LazySpec
+return {
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.blade" },
+  {
+    "adalessa/laravel.nvim",
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "nvimtools/none-ls.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          local prefix = "<Leader>L"
+          maps.n[prefix] = { desc = require("astroui").get_icon("Laravel", 1, true) .. "Laravel" }
+
+          maps.n[prefix .. "a"] = { ":Laravel artisan<CR>", desc = "Artisan" }
+          maps.n[prefix .. "r"] = { ":Laravel routes<CR>", desc = "Routes" }
+          maps.n[prefix .. "c"] = { ":Composer<CR>", desc = "Composer" }
+          maps.n[prefix .. "n"] = { ":Npm<CR>", desc = "Npm" }
+          maps.n[prefix .. "y"] = { ":Yarn<CR>", desc = "Yarn" }
+        end,
+      },
+      { "AstroNvim/astroui", opts = { icons = { Laravel = "󰫐" } } },
+    },
+
+    event = { "VeryLazy" },
+    config = true,
+  },
+  {
+    "Bleksak/laravel-ide-helper.nvim",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          local prefix = "<Leader>Li"
+          maps.n[prefix] = { desc = "Laravel Ide Helper" }
+
+          maps.n[prefix .. "m"] = {
+            function() require("laravel-ide-helper").generate_models(vim.fn.expand "%") end,
+            desc = "Generate Model Info for current model",
+          }
+          maps.n[prefix .. "M"] = {
+            function() require("laravel-ide-helper").generate_models() end,
+            desc = "Generate Model Info for all models",
+          }
+        end,
+      },
+    },
+    opts = {
+      write_to_models = true,
+      save_before_write = true,
+      format_after_gen = true,
+    },
+    enabled = function() return vim.fn.filereadable "artisan" ~= 0 end,
+  },
+  {
+    "ricardoramirezr/blade-nav.nvim",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    ft = { "blade", "php" },
+  },
+}
