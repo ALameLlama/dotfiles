744ac87fd20aac2492fe1e187fb5500bad07aa2f
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2024-07-19 15:55:41 +0000

    fix(htmx): htmx lsp

--- .config/nvim/lua/plugins/htmx.lua
+++ .config/nvim/lua/plugins/htmx.lua
@@ -3,11 +3,23 @@ return {
   {
     "ThePrimeagen/htmx-lsp",
     ft = { "htmx" },
+    specs = {
+      "williamboman/mason-lspconfig.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "htmx" })
+      end,
+    },
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "htmx" },
-    },
+    "AstroNvim/astrolsp",
+    ---@param opts AstroLSPOpts
+    opts = function(_, opts)
+      opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+        htmx = {
+          filetypes = { "blade", "html" },
+        },
+      })
+    end,
   },
 }
