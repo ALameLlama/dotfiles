211f65fe0f2a6c1b560521f214c0acf0c82ce940
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2024-07-19 13:19:18 +0000

    feat(ai): hide copilot when cmp is open

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,3 +1,9 @@
+local cmp = require "cmp"
+
+cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true end)
+cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
+
+---@type LazySpec
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
