0722c7465e7825c247ef90742e8157d9603ce8cb
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-10-05 12:53:10 +0000

    feat(community): add more plugins

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -34,10 +34,34 @@ return {
   -- { import = "astrocommunity.editing-support.bigfile-nvim" }, -- Need to see if this is better then astrocore large_buf
   { import = "astrocommunity.editing-support.copilotchat-nvim" }, -- Copilot chat,
   { import = "astrocommunity.editing-support.cutlass-nvim" }, -- Cutlass overrides the delete operations to actually just delete and not affect the current yank.
+  { import = "astrocommunity.editing-support.hypersonic-nvim" },
   { import = "astrocommunity.editing-support.mini-splitjoin" }, -- Split/Join params with gS
-  { import = "astrocommunity.editing-support.multiple-cursors-nvim" }, -- Multi cursor
+  { import = "astrocommunity.editing-support.neogen" }, -- Annotation generator
+  { import = "astrocommunity.editing-support.nvim-context-vt" }, -- Virtual Text for end statements
+  { import = "astrocommunity.editing-support.nvim-origami" }, -- Code Folding
   { import = "astrocommunity.editing-support.nvim-treesitter-context" }, -- Sticy scroll
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" }, -- Rainbow brackets
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.suda-vim" }, -- Sudo write
+  { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+  { import = "astrocommunity.editing-support.text-case-nvim" },
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
+
+  -- Git
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.git.gitgraph-nvim" },
+  { import = "astrocommunity.git.gitlinker-nvim" },
+
+  -- LSP
+  { import = "astrocommunity.lsp.actions-preview-nvim" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+
+  -- Markdown
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
 
   -- Packs
   { import = "astrocommunity.pack.bash" },
@@ -52,13 +76,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.yaml" },
-
-  -- Markdown
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
-
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.gitgraph-nvim" },
 
   -- Other
   { import = "astrocommunity.neovim-lua-development.helpview-nvim" },
