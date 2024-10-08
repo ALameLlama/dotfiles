1c23ffdb6b79c5660ccc0787f8693c65ef0a5969
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-10-09 09:40:49 +1100

    feat(personal): try out magazine.nvim

--- .config/nvim/lua/plugins/personal.lua
+++ .config/nvim/lua/plugins/personal.lua
@@ -8,4 +8,32 @@ return {
     },
     opts = {},
   },
+  -- {
+  --   "Saghen/blink.cmp",
+  --   event = "InsertEnter",
+  --   version = "v0.*",
+  --   dependencies = { "rafamadriz/friendly-snippets" },
+  --   opts = {
+  --     highlight = { use_nvim_cmp_as_default = true },
+  --     windows = {
+  --       autocomplete = {
+  --         border = "rounded",
+  --       },
+  --       documentation = {
+  --         border = "rounded",
+  --       },
+  --     },
+  --   },
+  --   specs = {
+  --     -- disable built in completion plugins
+  --     { "hrsh7th/nvim-cmp", enabled = false },
+  --     { "rcarriga/cmp-dap", enabled = false },
+  --     { "L3MON4D3/LuaSnip", enabled = false },
+  --     { "onsails/lspkind.nvim", enabled = false },
+  --   },
+  -- },
+  {
+    "iguanacucumber/magazine.nvim",
+    name = "nvim-cmp",
+  },
 }
