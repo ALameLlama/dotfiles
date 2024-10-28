daca44b4b8ffed5129357c3d70eb03ff02df8968
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2024-10-29 09:40:22 +1100

    feat(community): tiny-inline-diagnostic-nvim

--- .config/nvim/lua/plugins/personal.lua
+++ .config/nvim/lua/plugins/personal.lua
@@ -32,18 +32,4 @@ return {
   --     { "onsails/lspkind.nvim", enabled = false },
   --   },
   -- },
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "astronvim/astrocore",
-      opts = {
-        features = {
-          -- Disable diagnostics virtual textto prevet duplicates
-          diagnostics_mode = 2,
-        },
-      },
-    },
-    opts = {},
-  },
 }
