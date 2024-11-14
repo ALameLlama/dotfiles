bf16b7b4f0c09441b2867cbb6e4332fd70ce5b5c
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2024-11-14 11:34:25 +0000

    feat: nightly neovim support

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -7,22 +7,31 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = {
     nixpkgs,
     home-manager,
     ...
-  }: let
+  } @ inputs: let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
     userConfig = builtins.fromJSON (builtins.readFile ./user-config.json);
+    overlays = [
+      # inputs.neovim-nightly-overlay.overlays.default
+    ];
   in {
     homeConfigurations = {
       "${userConfig.username}" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [./home.nix];
+        modules = [
+          ./home.nix
+          {
+            nixpkgs.overlays = overlays;
+          }
+        ];
       };
     };
   };
