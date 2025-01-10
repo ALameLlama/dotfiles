521b171b6b22276c72e57fba587ca1b093d8b448
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-01-10 16:01:37 +1100

    fix: dfs alias sometimes failing because fuck you that's why

--- .config/home-manager/programs.nix
+++ .config/home-manager/programs.nix
@@ -27,6 +27,8 @@ with pkgs; {
 
       export LUA_PATH="$HOME/.luarocks/share/lua/5.1/?.lua;$HOME/.luarocks/share/lua/5.1/?/init.lua;./?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;/usr/local/lib/lua/5.1/?.lua;/usr/local/lib/lua/5.1/?/init.lua;/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua"
       export LUA_CPATH="$HOME/.luarocks/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/?.so;/usr/lib/x86_64-linux-gnu/lua/5.1/?.so;/usr/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so"
+
+      source ~/.bash_aliases
     '';
     shellAliases = {
       ".." = "cd ..";
@@ -46,7 +48,7 @@ with pkgs; {
 
       # Aliases for directories
       dfc = ''cd "$HOME/.dotfiles"'';
-      dfs = ''(cd "$HOME/.dotfiles" && home-manager switch)'';
+      dfs = "home-manager switch -f ~/.dotfiles/.config/home-manager/home.nix";
       dfu = ''(cd "$HOME/.dotfiles/.config/home-manager" && nix flake update)'';
       dfg = ''(cd "$HOME/.dotfiles" && lazygit)'';
       dfn = ''(cd "$HOME/.dotfiles" && nvim .)'';
