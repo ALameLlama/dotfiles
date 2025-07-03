19b08a8cb0e6a8c63472e6d917241ca83838cefc
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2025-07-03 23:25:05 +1000

    feat: set default zsh

--- users/nciechanowski/home.nix
+++ users/nciechanowski/home.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }: {
-  imports = [
-    ../../home
-    ../../home/programs/shell
-    ../../home/programs/tmux
-    ../../home/programs/cli-tooling
-    ../../home/programs/neovim
-    ../../home/programs/wezterm
-  ];
+{ pkgs, ... }:
+let username = "nciechanowski";
+in {
+  programs.zsh.enable = true;
+  users.users.${username} = { shell = pkgs.zsh; };
+
+  home-manager.users.${username} = {
+    imports = [
+      ../../home
+      ../../home/programs/shell
+      ../../home/programs/tmux
+      ../../home/programs/cli-tooling
+      ../../home/programs/neovim
+      ../../home/programs/wezterm
+    ];
+  };
 }
