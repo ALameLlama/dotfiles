db25df87ccfbcb5771ba894ede6a3fb739570a19
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-07-03 23:25:05 +1000

    feat: set default zsh

--- flake.nix
+++ flake.nix
@@ -29,13 +29,12 @@
           modules = [
             determinate.nixosModules.default
             ./hosts/framework16/configuration.nix
+            ./users/${username}/home.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} =
-                import ./users/${username}/home.nix;
             }
           ];
         };
