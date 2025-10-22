5076bc0fef70aed73763cd7dcc3342585abc9803
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-10-23 08:58:21 +1100

    chore: bump

--- home/programs/language/python/default.nix
+++ home/programs/language/python/default.nix
@@ -7,8 +7,8 @@
 with lib;
 {
   config = mkIf config.features.languages.python.enable {
-    home.packages = [
-      (pkgs.python313.withPackages (
+    home.packages = with pkgs; [
+      (python313.withPackages (
         p: with p; [
           playwright
           pip
