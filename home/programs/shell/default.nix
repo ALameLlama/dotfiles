148dc16cf1cfba09b322c6f26eca7da7cfbd5659
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-11-06 13:58:22 +1100

    feat: update editor to nvim

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -150,7 +150,7 @@
           # export LUA_PATH="$HOME/.luarocks/share/lua/5.1/?.lua;$HOME/.luarocks/share/lua/5.1/?/init.lua;./?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;/usr/local/lib/lua/5.1/?.lua;/usr/local/lib/lua/5.1/?/init.lua;/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua"
           # export LUA_CPATH="$HOME/.luarocks/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/?.so;/usr/lib/x86_64-linux-gnu/lua/5.1/?.so;/usr/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so"
 
-          export EDITOR="neovim"
+          export EDITOR="nvim"
         '';
         shellAliases = {
           ".." = "cd ..";
