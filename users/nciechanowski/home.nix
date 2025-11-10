de230478a4508ca3f2126b0e79507237c8d9acc6
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-11-10 21:08:23 +1100

    feat: make fnm a separate toggle

--- users/nciechanowski/home.nix
+++ users/nciechanowski/home.nix
@@ -11,8 +11,38 @@ in
   home-manager.users.${username} = {
     imports = [
       ../../home
-      ../../home/profiles/full.nix
+      # ../../home/profiles/full.nix
     ];
+
+    features = {
+      programs = {
+        neovim.enable = true;
+        tmux.enable = true;
+        shell.enable = true;
+        cli-tooling.enable = true;
+        wezterm.enable = true;
+        git.enable = true;
+      };
+
+      languages = {
+        php.enable = false;
+        python.enable = false;
+        javascript = {
+          enable = true;
+          fnm.enable = false;
+        };
+        rust.enable = true;
+        lua.enable = true;
+        go.enable = true;
+        zig.enable = true;
+      };
+
+      tools = {
+        nix-tools.enable = true;
+        utilities.enable = true;
+        fonts.enable = true;
+      };
+    };
 
     home.packages = with pkgs; [
       # wtfutil
