5dcb22ad38803b8ad4d3e3c4a3458c2cc8f3496a
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2026-02-03 16:46:18 +1100

    feat: add nixpkgs-update

--- home/utilities.nix
+++ home/utilities.nix
@@ -10,6 +10,16 @@
       dive
       opencode
       lsof
+      (pkgs.writeShellScriptBin "nixpkgs-update" ''
+        if [ -z "''${GITHUB_TOKEN-}" ]; then
+          echo "Error: GITHUB_TOKEN is not set. Please set it to a valid GitHub token." >&2
+          exit 1
+        fi
+        exec nix run \
+          --option extra-substituters https://nix-community.cachix.org \
+          --option extra-trusted-public-keys nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= \ 
+          github:nix-community/nixpkgs-update -- "$@"
+      '')
     ];
   };
 }
